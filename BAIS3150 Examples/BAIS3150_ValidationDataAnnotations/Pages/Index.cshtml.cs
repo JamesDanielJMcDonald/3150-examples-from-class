46e632e554954cc5d683fb0d159e23a230cb9069
James McDonald <31709623+Jmcdonald49@users.noreply.github.com>
2019-11-16 19:27:34 -0700

    BAIS 3150 examples from class

---
+++ BAIS3150 Examples/BAIS3150_ValidationDataAnnotations/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+
+namespace BAIS3150_ValidationDataAnnotations.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Message { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string Field { get; set; }
+
+        [Remote(action: "VerifyInput", controller: "Users", HttpMethod = "Post")]
+        [BindProperty]
+        public string ValidateMe { get; set; }
+
+        public void OnGet()
+        {
+            Message = "*** OnGet() ***";
+        }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                Message = "*** OnPost() *** - Valid";
+            }
+            else
+            {
+                Message = "*** OnPost() *** - Not Valid";
+            }
+        }
+    }
+}
---
+++ BAIS3150 Examples/Calculator/Pages/IndexBootstrap.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Calculator.Pages
+{
+    public class IndexBootstrapModel : PageModel
+    {
+        // BindProperty wires stuff up
+        [BindProperty]
+        public int First { get; set; }
+        [BindProperty]
+        public int Second { get; set; }
+        [BindProperty]
+        public string calcResult { get; set; }
+        // TempData encapsulates local data, the user won't see it (won't be pushed back to the form)
+        [TempData]
+        public int IntegerResult { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            IntegerResult = First + Second;
+
+            calcResult = IntegerResult.ToString();
+        }
+    }
+}
